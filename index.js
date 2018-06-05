af818c4e47a5b426d1cab45fbaaa6f1f75feb828
shenjia <jshenabc@gmail.com>
2018-06-05 00:02:44 -0400

    add lib code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function createStore () {
+// Library Code
+function createStore (reducer) {
   // The store should have four parts
   // 1. The state
   // 2. Get the state.
@@ -6,10 +7,25 @@ function createStore () {
   // 4. Update the state
 
   let state
+  let listeners = []
 
   const getState = () => state
 
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
   return {
-    getState
+    getState,
+    subscribe,
+    dispatch,
   }
 }
