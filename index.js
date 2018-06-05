8d54fb7fa20c10e203d3fee3b65b4784162124dd
shenjia <jshenabc@gmail.com>
2018-06-05 17:57:16 -0400

    handle different actions
    As of right now, our code is handling the ADD_TODO action. There are still a couple more actions that our app is supposed to be able to handle:
    
    the REMOVE_TODO action
    the TOGGLE_TODO action

--- index.js
+++ index.js
@@ -32,11 +32,31 @@ function createStore (reducer) {
 
 // App Code
 function todos (state = [], action) {
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.todo])
+      //break;
+    case 'REMOVE_TODO':
+      return state.filter((todo) => todo.id !== action.id)
+      //break;
+    case 'TOGGLE_TODO':
+      return state.map((todo) => todo.id !== action.id ? todo : Object.assign({}, todo, {complete: !todo.complete}))
+      //break;
+    default:
+      return state
   }
 
-  return state
+  //use switch instead to clean up code
+  // if (action.type === 'ADD_TODO') {
+  //   return state.concat([action.todo])
+  // } else if (action.type === 'REMOVE_TODO') {
+  //   return state.filter((todo) => todo.id !== action.id)
+  // } else if (action.type === 'TOGGLE_TODO') {
+  //   return state.map((todo) => todo.id !== action.id ? todo : Object.assign({}, todo, {complete: !todo.complete}))
+  // } else {
+  //   return state
+  // }
+
 }
 
 const store = createStore(todos);
